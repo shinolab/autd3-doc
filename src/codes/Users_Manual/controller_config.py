bb8d06fdce85117d2583ff09c965d0fa90ac5f5d
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-25 08:16:59 +0900

    update python

--- src/codes/Users_Manual/controller_config.py
+++ src/codes/Users_Manual/controller_config.py
@@ -1,7 +1,9 @@
 ~from datetime import timedelta
 ~from pyautd3 import Controller, AUTD3
+~from pyautd3.controller.timer import TimerStrategy, SpinSleeper
 Controller.builder([AUTD3([0.0, 0.0, 0.0])])\
-    .with_parallel_threshold(4)\
+    .with_fallback_parallel_threshold(4)\
+    .with_fallback_timeout(timedelta(milliseconds=20))\
     .with_send_interval(timedelta(milliseconds=1))\
     .with_receive_interval(timedelta(milliseconds=1))\
-    .with_timer_resolution(1)
+    .with_timer_strategy(TimerStrategy.Spin(SpinSleeper()))
