c213bda823544bcce2922f06b177e964680789ed
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-25 08:16:59 +0900

    update python

--- src/codes/Users_Manual/link/soem_0.py
+++ src/codes/Users_Manual/link/soem_0.py
@@ -3,28 +3,21 @@
 from pyautd3.link.soem import SOEM, Status, TimerStrategy, ThreadPriority, ProcessPriority
 
 
-def err_handler(slave: int, status: Status, msg: str) -> None:
-    match status:
-        case Status.Error:
-            print(f"Error [{slave}]: {msg}")
-        case Status.Lost:
-            print(f"Lost [{slave}]: {msg}")
-            # You can also wait for the link to recover, without exiting the process
-            os._exit(-1)
-        case Status.StateChanged:
-            print(f"StateChanged  [{slave}]: {msg}")
+def err_handler(slave: int, status: Status) -> None:
+    print(f"slave [{slave}]: {status}")
+    if status == Status.Lost():
+        # You can also wait for the link to recover, without exiting the process
+        os._exit(-1)
 
 
 SOEM.builder()\
     .with_ifname("")\
     .with_buf_size(32)\
-    .with_err_handler(
-        err_handler
-    )\
+    .with_err_handler(err_handler)\
     .with_state_check_interval(timedelta(milliseconds=100))\
-    .with_sync0_cycle(2)\
-    .with_send_cycle(2)\
-    .with_timer_strategy(TimerStrategy.BusyWait)\
+    .with_sync0_cycle(timedelta(milliseconds=1))\
+    .with_send_cycle(timedelta(milliseconds=1))\
+    .with_timer_strategy(TimerStrategy.SpinSleep)\
     .with_sync_tolerance(timedelta(microseconds=1))\
     .with_sync_timeout(timedelta(seconds=10))\
     .with_thread_priority(ThreadPriority.Max)\
