c2776b78f37daed525278b1b39e4b106ddb95a64
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-28 09:26:03 +0900

    update

--- src/codes/Users_Manual/controller_config.cs
+++ src/codes/Users_Manual/controller_config.cs
@@ -1,6 +1,6 @@
 ~using System;
 ~using AUTD3Sharp;
-~using AUTD3Sharp.Link;
+~using AUTD3Sharp.Timer;
 ~using AUTD3Sharp.Utils;
 ~_ =
 Controller.Builder([new AUTD3(Vector3.Zero)])
@@ -8,4 +8,5 @@ Controller.Builder([new AUTD3(Vector3.Zero)])
     .WithFallbackTimeout(TimeSpan.FromMilliseconds(20))
     .WithSendInterval(TimeSpan.FromMilliseconds(1))
     .WithReceiveInterval(TimeSpan.FromMilliseconds(1))
+    .WithTimerStrategy(AUTD3Sharp.Timer.TimerStrategy.Spin(new SpinSleeper()))
 ~;
--- src/codes/Users_Manual/controller_config.py
+++ src/codes/Users_Manual/controller_config.py
@@ -1,6 +1,7 @@
 ~from datetime import timedelta
 ~from pyautd3 import Controller, AUTD3
-~from pyautd3.controller.timer import TimerStrategy, SpinSleeper
+from pyautd3.controller.timer import TimerStrategy, SpinSleeper
+
 Controller.builder([AUTD3([0.0, 0.0, 0.0])])\
     .with_fallback_parallel_threshold(4)\
     .with_fallback_timeout(timedelta(milliseconds=20))\
