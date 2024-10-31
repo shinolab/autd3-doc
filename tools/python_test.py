51b07e4a768e8850d0f503d5abe589b1804c4ed6
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-31 12:50:44 +0900

    update python and cs version

--- tools/python_test.py
+++ tools/python_test.py
@@ -23,7 +23,7 @@ def python_module(cmd):
 
 
 def install_pyautd3():
-    version = "29.0.0rc3.post3"
+    version = "29.0.0rc3.post4"
     subprocess.run(
         python_module(["pip", "install", "-U", f"pyautd3=={version}"]),
         capture_output=True,
