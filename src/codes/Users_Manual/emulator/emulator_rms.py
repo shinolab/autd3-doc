3db0c6a8c7b9e8e773aea0b0a91890507852ed80
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-11-11 17:23:14 +0900

    add rms

---
+++ src/codes/Users_Manual/emulator/emulator_rms.py
@@ -0,0 +1,33 @@
+~import numpy as np
+from pyautd3 import AUTD3, Controller, Duration, Focus, Silencer, Static
+from pyautd3.emulator import Range, Recorder, RmsRecordOption
+
+with Controller.builder([AUTD3([0.0, 0.0, 0.0])]).into_emulator() as emulator:
+    focus = emulator.center + np.array([0.0, 0.0, 150.0])
+
+    def f(autd: Controller[Recorder]) -> Controller[Recorder]:
+        autd.send(Silencer.disable())
+        autd.send((Static.with_intensity(0xFF), Focus(focus)))
+        autd.tick(Duration.from_micros(25))
+        return autd
+
+    record = emulator.record(f)
+
+    sound_field = record.sound_field(
+        Range(
+            x_start=focus[0] - 20.0,
+            x_end=focus[0] + 20.0,
+            y_start=focus[1] - 20.0,
+            y_end=focus[1] + 20.0,
+            z_start=focus[2],
+            z_end=focus[2],
+            resolution=1.0,
+        ),
+        RmsRecordOption(
+            sound_speed=340e3,
+            print_progress=True,
+            gpu=False,
+        ),
+    )
+    df = sound_field.next(Duration.from_micros(25))
+    print(df)
---
+++ src/codes/Users_Manual/emulator/emulator_rms.rs
@@ -0,0 +1,40 @@
+# use std::time::Duration;
+# use autd3::prelude::*;
+use autd3_emulator::*;
+
+# #[tokio::main]
+# async fn main() -> Result<(), Box<dyn std::error::Error>> {
+let emulator = Controller::builder([AUTD3::new(Vector3::zeros())]).into_emulator();
+
+let focus = emulator.center() + Vector3::new(0., 0., 150. * mm);
+
+let record = emulator
+    .record(|mut autd| async {
+        autd.send(Silencer::disable()).await?;
+        autd.send((Static::with_intensity(0xFF), Focus::new(focus)))
+            .await?;
+        autd.tick(Duration::from_micros(25))?;
+        Ok(autd)
+    })
+    .await?;
+
+let mut sound_field = record
+    .sound_field(
+        Range {
+            x: focus.x - 20.0..=focus.x + 20.0,
+            y: focus.y - 20.0..=focus.y + 20.0,
+            z: focus.z..=focus.z,
+            resolution: 1.,
+        },
+        RmsRecordOption {
+            sound_speed: 340e3 * mm,
+            print_progress: true,
+            gpu: true,        
+        },
+    )
+    .await?;
+   
+let df = sound_field.next(Duration::from_micros(25)).await?;
+dbg!(df);
+#     Ok(())
+# }
