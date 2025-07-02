59bda7ca83a7548e8fd2214bd2471f80113958d3
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-07-02 15:39:10 +0900

    fix py

--- src/codes/Users_Manual/Tutorial/move_focus/hard.py
+++ src/codes/Users_Manual/Tutorial/move_focus/hard.py
@@ -18,7 +18,7 @@ autd.send(
         ],
         config=0.5 * Hz,
         # config=Duration.from_secs(2),
-        # config=SamplingConfig(0.5 * Hz),
+        # config=SamplingConfig(1.0 * Hz),
         # config=SamplingConfig(Duration.from_secs(1)),
     )
 )
