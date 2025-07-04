640b4abb2fdd915c64991a16d652d8aa74e07e44
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-07-04 09:09:27 +0900

    update cpp

--- src/codes/Users_Manual/environment.cpp
+++ src/codes/Users_Manual/environment.cpp
@@ -4,8 +4,8 @@
 //~using namespace autd3;
 //~auto autd =
 //~Controller::open({AUTD3{}}, link::Nop{});
-const auto sound_speed = autd.environment.sound_speed();
-autd.environment.set_sound_speed(340e3);
+const float sound_speed = autd.environment.sound_speed;
+autd.environment.sound_speed = 340e3;
 autd.environment.set_sound_speed_from_temp(15.);
 const auto wavelength = autd.environment.wavelength();
 const auto wavenumber = autd.environment.wavenumber();
