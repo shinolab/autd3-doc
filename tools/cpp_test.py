998b0dc06d237d44377fb07b1c92dab4679effd2
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-11-20 13:43:01 +0900

    update C++

--- tools/cpp_test.py
+++ tools/cpp_test.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
 
 project(autd3-doc-test)
 
-set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
